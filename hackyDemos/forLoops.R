247249ce6b13bbb4eb5cb9e1138cd51d0b97da5a
Holly Kirk <hollylkirk@gmail.com>
2020-05-28 10:29:30 +0800

    adds for loop demonstration script

---
+++ hackyDemos/forLoops.R
@@ -0,0 +1,32 @@
+
+# demonstrating for loops
+
+# maks some data
+a <- 1:20
+b <- 2:21
+
+# demonstrate R's ability to do
+# vectorised computations
+d <- a + b
+
+
+# same thing using a for loop
+# for loop that loops over all items in a
+# and adds them to the i'th item in b
+names <- 1:20
+
+for(i in 1:length(names))  {
+  thing <- a + b
+  d <- c * b
+
+}
+
+
+# demonstrating how the apply function works
+df <- data.frame(a, b)
+df$e <- apply(df, 2, FUN = "sum")
+
+
+
+
+
