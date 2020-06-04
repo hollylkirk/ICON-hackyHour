effc751af1272b7e33df89bea1a3d8962a8f1090
Holly Kirk <hollylkirk@gmail.com>
2020-06-04 11:42:39 +0800

    adds a real life example

--- hackyDemos/forLoops.R
+++ hackyDemos/forLoops.R
@@ -31,3 +31,43 @@ df$e <- apply(df, 2, FUN = "sum")
 
 
 
+# REAL LIFE EXAMPLE
+library(here)
+library(GeoLight)
+library(FLightR)
+library(dplyr)
+
+
+# read txt files with names of the form '.lux'
+trj_files = list.files(here("data/rawData/"), pattern = '.lux')
+
+# folder namefor outputs
+outfolder <- here("data/derivedData/")
+
+
+# code for preparing the data for FlightR function
+for (i in 1:length(trj_files)) {
+  
+  fn <- trj_files[[i]]
+  bird <- substr(fn, 1,5)
+  
+  # load raw Lux files
+  f <- GeoLight::luxTrans(here("data/rawData/", fn))
+  
+  # use GEOlight package function twilightCalc to find the times for twilight
+  # use light threshold 10
+  twi <- twilightCalc(f$datetime, f$light,
+                      LightThreshold = 10,
+                      ask = FALSE,
+                      allTwilights = FALSE)
+  
+  #create a tag file for use in FlightR package
+  tagfile <- GeoLight2TAGS(f, twi, threshold = 10)
+  
+  # create a tag file
+  fname <- paste0(bird, "_tagfile.csv") 
+  write.csv(tagfile, file = paste(outfolder, fname, sep= '/'), row.names = FALSE)
+  
+}
+
+
